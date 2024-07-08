ea920d9f166bdd2a8b8aa7de71cd6e0374f85f84
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-08 15:17:42 +0200

    Initialize logic needed to draw

--- scripts.js
+++ scripts.js
@@ -3,13 +3,19 @@ const BOARD_HEIGHT = 500;
 const BOARD_WIDTH = 1200;
 const ctx = board.getContext("2d");
 
+const curX = 600;
+const curY = 250;
+
+// const stopUp = window.setInterval(100, drawUp);
+// const stopDown = window.setInterval(100, drawDown);
+// const stopLeft = window.setInterval(100, drawLeft);
+// const stopRight = window.setInterval(100, drawRight);
+
 board.setAttribute("width", BOARD_WIDTH)
 board.setAttribute("height", BOARD_HEIGHT)
-
-
 ctx.fillStyle = "black";
-ctx.fillRect(100, 100, 1, 1)
-ctx.fillRect(20, 100, 10, 10)
+
+ctx.fillRect(curX, curY, 1, 1)
 
 
 
